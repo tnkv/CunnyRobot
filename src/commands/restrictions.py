d6f0bb2158349e93bb470cfb4babee89395c749d
tnkv <75366889a@gmail.com>
2023-08-24 13:13:23 +0300

    ban, unmute by telegram user id

--- src/commands/restrictions.py
+++ src/commands/restrictions.py
@@ -22,7 +22,15 @@ async def command_ban(message: Message):
         await message.chat.ban(user_id=message.reply_to_message.from_user.id)
         return
 
-    await message.reply("А ответить на сообщение?")
+    msg = message.text.split(" ")
+    if len(msg) >= 2 and msg[1].isdigit():
+        try:
+            await message.chat.ban(user_id=int(msg[1]))
+        except Exception:
+            await message.reply("Не удалось заблокировать пользователя.")
+        return
+    await message.reply(
+        "Для блокировки пользователя необходимо ответить на сообщение или написать Telegram ID через пробел.")
 
 
 # Мут
@@ -57,27 +65,35 @@ async def command_mute(message: Message):
         await message.reply("Ты не админ.")
         return
 
-    if not message.reply_to_message:
-        await message.reply("А ответить на сообщение?")
-        return
-
-    await message.chat.restrict(user_id=message.reply_to_message.from_user.id,
-                                until_date=0,
-                                permissions=ChatPermissions(can_send_messages=True,
-                                                            can_pin_messages=True,
-                                                            can_send_other_messages=True,
-                                                            can_send_polls=True,
-                                                            can_change_info=True,
-                                                            can_invite_users=True,
-                                                            can_send_audios=True,
-                                                            can_send_photos=True,
-                                                            can_send_videos=True,
-                                                            can_manage_topics=True,
-                                                            can_send_documents=True,
-                                                            can_send_video_notes=True,
-                                                            can_send_voice_notes=True,
-                                                            can_add_web_page_previews=True))
-
+    if message.reply_to_message:
+        TelegramID = message.reply_to_message.from_user.id
+    else:
+        msg = message.text.split(" ")
+        if len(msg) >= 2 and msg[1].isdigit():
+            TelegramID = int(msg[1])
+        else:
+            await message.reply(
+                "Для снятия ограничений необходимо ответить на сообщение или написать Telegram ID через пробел.")
+            return
+    try:
+        await message.chat.restrict(user_id=TelegramID,
+                                    until_date=0,
+                                    permissions=ChatPermissions(can_send_messages=True,
+                                                                can_pin_messages=True,
+                                                                can_send_other_messages=True,
+                                                                can_send_polls=True,
+                                                                can_change_info=True,
+                                                                can_invite_users=True,
+                                                                can_send_audios=True,
+                                                                can_send_photos=True,
+                                                                can_send_videos=True,
+                                                                can_manage_topics=True,
+                                                                can_send_documents=True,
+                                                                can_send_video_notes=True,
+                                                                can_send_voice_notes=True,
+                                                                can_add_web_page_previews=True))
+    except Exception:
+        await message.reply("Не удалось снять огранчиения.")
 
 @router.message(Command(commands=["is_cas_ban"]))
 async def command_mute(message: Message):
@@ -90,6 +106,7 @@ async def command_mute(message: Message):
         return
     await message.reply(f"Статус блокировки в CAS: {await isCasBan(int(msg[1]))}")
 
+
 # Перевод времени для темпмута
 def getRestrictTime(duration):
     unit = duration[-1]
@@ -98,6 +115,7 @@ def getRestrictTime(duration):
     value = int(duration[:-1])
     coefficient = {"m": 60, "h": 3600, "d": 86400}
     return int(time()) + value * coefficient[unit]
+
 
 # Проверка наличия пользователя в базе CAS
 async def isCasBan(TelegramUserID: int) -> bool:
