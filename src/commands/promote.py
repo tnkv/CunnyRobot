b5a2a45112be423157a699c3db8f7f33c79f20ea
Igor Tankov <67426242+tnkv@users.noreply.github.com>
2023-08-25 21:55:20 +0300

    new alias to immune commands

--- src/commands/promote.py
+++ src/commands/promote.py
@@ -12,7 +12,7 @@ bot = Bot(token=os.getenv('TOKEN'), parse_mode="HTML")
 router = Router()
 
 # Выдача иммунитета от трибунала (У всех админов он по умолчанию потому что их замутить нельзя)
-@router.message(Command(commands=["give_immune"]))
+@router.message(Command(commands=["give_immune", "give_immunity"]))
 async def command_give_admin(message: Message) -> None:
     initiator = (await message.chat.get_member(user_id=message.from_user.id)).status
     if initiator not in (ChatMemberStatus.CREATOR, ChatMemberStatus.ADMINISTRATOR):
@@ -43,8 +43,8 @@ async def command_check(message: Message) -> None:
         f"Наличие иммунитета к трибуналу: {await database.isImmune(message.chat.id, message.reply_to_message.from_user.id)}")
 
 # Отмена иммунитета
-@router.message(Command(commands=["revoke_immune"]))
-async def command_give_admin(message: Message) -> None:
+@router.message(Command(commands=["revoke_immune", "revoke_immunity"]))
+async def command_revoke_admin(message: Message) -> None:
     initiator = (await message.chat.get_member(user_id=message.from_user.id)).status
     if initiator not in (ChatMemberStatus.CREATOR, ChatMemberStatus.ADMINISTRATOR):
         await message.reply("Ты не админ.")
