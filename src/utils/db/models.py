bad3ae90e7cd563bab04c927d2d5013726f41765
Igor Tankov <igor@tnkv.ru>
2024-01-03 12:28:10 +0300

    database warning table

--- src/utils/db/models.py
+++ src/utils/db/models.py
@@ -1,6 +1,6 @@
 import json
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 import config
 from src.utils.db.base import Base
@@ -11,4 +11,13 @@ class TribunalBot(Base):
     TelegramChatID = Column(Integer, primary_key=True, unique=True, autoincrement=False)
     LastTribunalEnd = Column(Integer, default=0)
     ChatSettings = Column(String, default=json.dumps(config.DEFAULT_CHAT_SETTINGS))
+
+class Warns(Base):
+    __tablename__ = 'Warns'
+    WarnID = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    TelegramChatID = Column(Integer, ForeignKey('TribunalBot.TelegramChatID'))
+    TelegramUserID = Column(Integer, default=0)
+    Reason = Column(String, default='')
+    MessageID = Column(Integer, default=0)
+    IsActive = Column(Boolean, default=True)
 
