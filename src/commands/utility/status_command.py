7be82cb0af1e7288f73f3cbfa6fdb7450c2609c1
Igor Tankov <igor@tnkv.ru>
2024-01-10 20:15:53 +0300

    move getting info from functions to constants where possible (status_command.py)

--- src/commands/utility/status_command.py
+++ src/commands/utility/status_command.py
@@ -13,32 +13,27 @@ from src.utils import database, filters
 
 router = Router()
 
-startup_time = datetime.now()
-
+STARTUP_TIME = datetime.now()
+repo = git.Repo(search_parent_directories=True)
+COMMIT = repo.git.rev_parse(repo.head, short=True)
+OS = f'{platform.system()} {platform.release()} ({platform.version()})'
+CPU = f'{psutil.cpu_count()} ({psutil.cpu_freq().max / 1000:.2f}GHz)'
+RAM_AMOUNT = f'{round(psutil.virtual_memory().total / (1024.0 ** 3), 1)} GB'
 
 @router.message(Command(commands=['status']), filters.SuperUserFilter())
 async def command_status(message: Message, session: AsyncSession) -> None:
     await message.reply(
-        text=f'<b>TribunalBot</b> (<code>{get_commit()}</code>)\n\n'
+        text=f'<b>TribunalBot</b> (<code>{COMMIT}</code>)\n\n'
              f'Uptime: {format_uptime()}\n'
              f'Used RAM: {used_ram()}\n'
              f'Chat count: {await database.chat_count(session)}\n\n'
-             f'OS: {get_os()}\n'
-             f'CPU: {psutil.cpu_count()} ({psutil.cpu_freq().max / 1000:.2f}GHz)\n'
-             f'RAM: {get_ram()}\n\n'
-             f'Startup date: {format_time(startup_time)}\n'
+             f'OS: {OS}\n'
+             f'CPU: {CPU}\n'
+             f'RAM: {RAM_AMOUNT}\n\n'
+             f'Startup date: {format_time(STARTUP_TIME)}\n'
              f'System time: {format_time(datetime.now())}\n\n'
              f'<i>Powered by aiogram ({version("aiogram")})</i>\n'
-             f''
     )
-
-
-def get_os() -> str:
-    return f'{platform.system()} {platform.release()} ({platform.version()})'
-
-
-def get_ram() -> str:
-    return f'{round(psutil.virtual_memory().total / (1024.0 ** 3), 1)} GB'
 
 
 def used_ram() -> str:
@@ -51,15 +46,10 @@ def format_time(date: datetime) -> str:
 
 
 def format_uptime() -> str:
-    uptime = datetime.now() - startup_time
+    uptime = datetime.now() - STARTUP_TIME
 
     days = uptime.days
     hours, remainder = divmod(uptime.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
 
     return f"{days}d {hours}h {minutes}m {seconds}s"
-
-
-def get_commit() -> str:
-    repo = git.Repo(search_parent_directories=True)
-    return repo.git.rev_parse(repo.head, short=True)
